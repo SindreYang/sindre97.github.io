fb4ace6e60f9eeffcc43d35bca84b615df10e405
sindre <sindre1997@sina.com>
2020-03-03 01:37:45 +0800

    Site updated: 2020-03-03 01:37:45

---
+++ about/js/password.js
@@ -0,0 +1,13 @@
+
+
+function checkpassword(){
+      var password = prompt("请输入您访问密码：", "如不知道密码,请联系我");
+      var pwd="sindre";
+      if ( password!=pwd ) {
+            alert("密码错误!！如有任何问题,请联系我!");
+            window.location.href="https://blog.yx1024.top/about/";
+
+      }
+
+      }
+
